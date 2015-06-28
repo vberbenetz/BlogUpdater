6bf44b1833568a17ecd63e17aa6db9b085df3214
Val <v.berbenetz@gmail.com>
2015-06-28 15:46:04 -0400

    Added google analytics. Added social buttons to post.

--- app/controllers/blog_controller.js
+++ app/controllers/blog_controller.js
@@ -29,95 +29,177 @@ blogController.prototype = {
             return;
         }
 
-        var gitAdded = filterMd(req.body.head_commit.added);
-        var gitRemoved = filterMd(req.body.head_commit.removed);
-        var gitModified = filterMd(req.body.head_commit.modified);
-
-
-
         // Update Posts from Github
         var execute = sh.exec(__dirname + '/../../scripts/./fetch_posts.sh');
 
-        // Get all post titles from DB
-        db.getPostTitles(function(err, results) {
-            if (err) {
-                res.send(500, 'Server Error.');
-                return;
+        var commits = req.body.commits;
+        var gitAdded, gitRemoved, gitModified;
+
+        // Get all added
+        for (var i = 0; i < commits.length; i++) {
+
+            // Skip if no additions in this commit
+            if (commits[i].added.length == 0) {
+                continue;
             }
 
-            var postFiles = fs.readdirSync(config.srcRoot + config.srcPostsDir);
+            for (var j = 0; j < commits[i].added.length; j++) {
 
-            var postsToAdd = filterNewPosts(results, postFiles);
+                var thisAdded = filterMd(commits[i].added[j]);
 
-            // Iterate through new md files and add to blog
-            for (var i = 0; i < postsToAdd.length; i++) {
+                // New addition
+                if (gitAdded.indexOf(thisAdded) > -1) {
+                    gitAdded.push(thisAdded);
+                }
+            }
+        }
 
-                // Encapsulate in function due to db callbacks within the loop
-                (function() {
+        // Get all removed
+        for (var i = 0; i < commits.length; i++) {
 
-                    // Get file contents as string
-                    var postFileContents = fs.readFileSync(config.srcRoot + config.srcPostsDir + postsToAdd[i], 'utf8');
+            // Skip if no removals in this commit
+            if (commits[i].removed.length == 0) {
+                continue;
+            }
 
-                    // Split new post based on summary tag
-                    var postPreview = postFileContents.split('<!--')[0];                  // Contents before summary tag
-                    var postMeta = postFileContents.split('<!--')[1].split('-->')[0];    // Contents within summary tag
+            for (var j = 0; j < commits[i].removed.length; j++) {
 
-                    // Extract Metadata
-                    var authorName = postMeta.split('|')[0].split('=')[1];
-                    var imgName = postMeta.split('|')[1].split('=')[1];
-                    var postTags = postMeta.split('|')[2].split('=')[1].split(",");
-                    var postTitle = postsToAdd[i].split(".")[0].replace(/-/g, ' ');
-                    var postDate = getCurrentDate();
+                var thisRemoved = filterMd(commits[i].removed[j]);
 
-                    // Convert post preview to HTML
-                    var converter = new pagedown.Converter();
-                    var htmlPreview = converter.makeHtml(postPreview);
+                // New addition
+                if (gitRemoved.indexOf(thisRemoved) > -1) {
+                    gitRemoved.push(thisRemoved);
+                }
+            }
+        }
 
-                    // Insert new post
-                    db.addNewPost(authorName, postDate, imgName, postTitle, htmlPreview, function(err, results) {
+        // Get all modified
+        for (var i = 0; i < commits.length; i++) {
+
+            // Skip if no modifications in this commit
+            if (commits[i].modified.length == 0) {
+                continue;
+            }
+
+            for (var j = 0; j < commits[i].modified.length; j++) {
+
+                var thisModified = filterMd(commits[i].modified[j]);
+
+                // New addition
+                if (gitModified.indexOf(thisModified) > -1) {
+                    gitModified.push(thisModified);
+                }
+            }
+        }
+
+
+        // Remove posts
+        for (var i = 0; i < gitRemoved.length; i++) {
+
+            (function() {
+                var postTitle = gitRemoved[i].split(".")[0].replace(/-/g, ' ');
+
+                db.removePost(postTitle, function(err, results) {
+
+                    if (err) {
+                        res.send(500, 'Server Error.');
+                        return;
+                    }
+
+                    // Delete md file
+                    fs.unlink(conf.destRoot + config.destPostsDir + gitRemoved, function (err) {
                         if (err) {
                             res.send(500, 'Server Error.');
                             return;
                         }
-
-                        // ------------- Create new post page ---------------- //
-
-                        // Get template HTML file contents
-                        var templateFileContents = fs.readFileSync(__dirname + '/../../post_template.html', 'utf8');
-
-                        // Insert post image path
-                        templateFileContents = templateFileContents.replace(/<!-- POST IMAGE -->/g, '<img class="img-responsive" src="./' + config.destImgsDir + imgName + '">');
-
-                        // Insert post title
-                        templateFileContents = templateFileContents.replace(/<!-- POST TITLE -->/g, postTitle);
-
-                        // Insert post author
-                        templateFileContents = templateFileContents.replace(/<!-- POST AUTHOR -->/g, authorName);
-
-                        // Insert post date
-                        templateFileContents = templateFileContents.replace(/<!-- POST DATE -->/g, postDate);
-
-                        // Convert post to HTML
-                        var htmlPostBody = converter.makeHtml(postFileContents);
-
-                        // Insert HTML post body into post page
-                        templateFileContents = templateFileContents.replace(/<!-- POST BODY -->/g, htmlPostBody);
-
-                        // Create new HTML post file
-                        var fd = fs.openSync(config.destRoot + config.destPostsDir + postTitle.replace(/\s+/g, '-') + '.html', 'a');
-                        fs.writeSync(fd, templateFileContents);
-
-                        // Copy post image file
-                        var copyImg = sh.exec('cp ' + config.srcRoot + config.srcImgsDir + imgName + ' ' + config.destRoot + config.destImgsDir);
                     });
 
-                })();
+                });
+            })();
 
-            }
+        }
+
+
+        var postsToAdd = gitAdded.concat(gitModified);
+
+        // Iterate through new md files and add to blog
+        for (var i = 0; i < postsToAdd.length; i++) {
+
+            // Encapsulate in function due to db callbacks within the loop
+            (function() {
+
+                // Get file contents as string
+                var postFileContents = fs.readFileSync(config.srcRoot + config.srcPostsDir + postsToAdd[i], 'utf8');
+
+                // Split new post based on summary tag
+                var postPreview = postFileContents.split('<!--')[0];                  // Contents before summary tag
+                var postMeta = postFileContents.split('<!--')[1].split('-->')[0];    // Contents within summary tag
+
+                // Extract Metadata
+                var authorName = postMeta.split('|')[0].split('=')[1];
+                var imgName = postMeta.split('|')[1].split('=')[1];
+                var postTags = postMeta.split('|')[2].split('=')[1].split(",");
+                var postTitle = postsToAdd[i].split(".")[0].replace(/-/g, ' ');
+                var postDate = getCurrentDate();
+
+                // Convert post preview to HTML
+                var converter = new pagedown.Converter();
+                var htmlPreview = converter.makeHtml(postPreview);
+
+                // Insert new post
+                db.addNewPost(authorName, postDate, imgName, postTitle, htmlPreview, function(err, results) {
+                    if (err) {
+                        res.send(500, 'Server Error.');
+                        return;
+                    }
+
+                    // ------------- Create new post page ---------------- //
+
+                    var postTitleHyphen = postTitle.replace(/\s+/g, '-');
+
+                    var postTitleUrlEncoded = postTitle.replace(/\s+/g, '%20');
+
+                    // Get template HTML file contents
+                    var templateFileContents = fs.readFileSync(__dirname + '/../../post_template.html', 'utf8');
+
+                    // Insert post title with hyphen for space
+                    templateFileContents = templateFileContents.replace(/<!-- POST TITLE HYPHEN -->/g, postTitleHyphen);
+
+                    // Insert post title with URL Encoded spaces
+                    templateFileContents = templateFileContents.replace(/<!-- POST TITLE URLENCODED -->/g, postTitleUrlEncoded);
+
+                    // Insert post image path
+                    templateFileContents = templateFileContents.replace(/<!-- POST IMAGE -->/g, '<img class="img-responsive" src="./' + config.destImgsDir + imgName + '">');
+
+                    // Insert post title
+                    templateFileContents = templateFileContents.replace(/<!-- POST TITLE -->/g, postTitle);
+
+                    // Insert post author
+                    templateFileContents = templateFileContents.replace(/<!-- POST AUTHOR -->/g, authorName);
+
+                    // Insert post date
+                    templateFileContents = templateFileContents.replace(/<!-- POST DATE -->/g, postDate);
+
+                    // Convert post to HTML
+                    var htmlPostBody = converter.makeHtml(postFileContents);
+
+                    // Insert HTML post body into post page
+                    templateFileContents = templateFileContents.replace(/<!-- POST BODY -->/g, htmlPostBody);
+
+                    // Create new HTML post file
+                    var fd = fs.openSync(config.destRoot + config.destPostsDir + postTitle.replace(/\s+/g, '-') + '.html', 'w');
+                    fs.writeSync(fd, templateFileContents);
+
+                    // Copy post image file
+                    var copyImg = sh.exec('cp ' + config.srcRoot + config.srcImgsDir + imgName + ' ' + config.destRoot + config.destImgsDir);
+                });
+
+            })();
+
+        }
 
 	    res.send(req.body);
 
-        });
     }
 
 };
@@ -129,15 +211,21 @@ function filterMd (listOfFiles) {
 
     for (var i = 0; i < listOfFiles.length; i++) {
 
-        var fileExtension = listOfFiles[i].substring(listOfFiles.lastIndexOf("."));
+        var fileExtension = listOfFiles[i].substring(listOfFiles[i].lastIndexOf("."));
 
-        console.log(fileExtension);
-
-        if (fileExtension != 'md') {
+        if (fileExtension != '.md') {
             listOfFiles.splice(i, 1);
             i--;
         }
+
+        // Split into file name
+        else {
+            var filename = listOfFiles[i].substring(listOfFiles[i].lastIndexOf("/") + 1);
+            listOfFiles[i] = filename;
+        }
     }
+
+    return listOfFiles;
 }
 
 function filterNewPosts (existingTitles, listOfFiles) {
